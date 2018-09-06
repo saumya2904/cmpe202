1f86eda3728b8c3181188c5fce1763971da5c97d
Saumya Goyal <saumya.iis@gmail.com>
2018-09-06 11:42:57 -0700

    Update GumballMachine.java

--- lab1/gumball-java-typical/GumballMachine.java
+++ lab1/gumball-java-typical/GumballMachine.java
@@ -35,3 +35,53 @@ public class GumballMachine
     }
     
 }
+public class GumballMachine1 extends GumballMachine {
+
+	public GumballMachine1(int size) {
+		super(size);
+	}
+	
+    public void insertMoney(int coin)
+    {
+        if ( coin == 25 )
+            this.has_money = true ;
+        else 
+            this.has_money = false ;
+    }
+
+}
+public class GumballMachine2 extends GumballMachine {
+
+	//private boolean has_money;
+	public GumballMachine2(int size) {
+		super(size);
+	}
+	public void insertMoney(int coin1, int coin2)
+	{
+		if(coin1==25&&coin1==coin2)
+		this.has_money=true;
+		else
+			this.has_money=false;
+		
+	}
+
+}
+public class GumballMachine3 extends GumballMachine {
+
+	public GumballMachine3(int size) {
+		super(size);
+	}
+	
+	public void insertMoney(int[] coins)
+	{
+		int sum=0;
+		for (int i=0; i<coins.length;i++)
+			sum= sum+coins[i];
+		if(sum==50||sum>50)
+		this.has_money=true;
+		else
+			this.has_money=false;
+		
+	}
+
+}
