662e49c8f2cb6ff55a6a0b923cc2d652c29f6978
Saumya Goyal <saumya.iis@gmail.com>
2018-09-08 19:50:00 -0700

    Create SoldOutState.java

---
+++ lab1/gumball-java-pattern/SoldOutState.java
@@ -0,0 +1,38 @@
+package cmpe202.cmpe202_2;
+
+public class SoldOutState implements State {
+    GumballMachine gumballMachine;
+ 
+    public SoldOutState(GumballMachine gumballMachine) {
+        this.gumballMachine = gumballMachine;
+    }
+ 
+	public void insertQuarter() {
+		System.out.println("You can't insert a quarter, the machine is sold out");
+	}
+ 
+	public void ejectQuarter() {
+		System.out.println("You can't eject, you haven't inserted a coin yet");
+	}
+ 
+	public void turnCrank() {
+		System.out.println("You turned, but there are no gumballs");
+	}
+ 
+	public void dispense() {
+		System.out.println("No gumball dispensed");
+	}
+ 
+	public String toString() {
+		return "sold out";
+	}
+
+	@Override
+	public void insertCoins(int[] coins) {
+		System.out.println("You can't insert more coins, the machine is sold out");
+		
+	}
+	public void insertQuarters(int a, int b) {
+		System.out.println("You can't insert more coins, the machine is sold out");
+	}
+}
