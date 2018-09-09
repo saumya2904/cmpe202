f4efdbcda295db59b98a78bec89a1d1a47733c64
Saumya Goyal <saumya.iis@gmail.com>
2018-09-08 19:44:39 -0700

    Create GumballMachine1.java

---
+++ lab1/gumball-java-pattern/GumballMachine1.java
@@ -0,0 +1,23 @@
+package cmpe202.cmpe202_2;
+
+public class GumballMachine1 extends GumballMachine {
+	
+	public GumballMachine1(int numberGumballs)
+	{
+		super(numberGumballs);
+	}
+	public void insertQuarter() {
+		state.insertQuarter();
+	}
+ 
+	public void ejectQuarter() {
+		state.ejectQuarter();
+	}
+ 
+	public void turnCrank() {
+		state.turnCrank();
+		state.dispense();
+	}
+	
+
+}
