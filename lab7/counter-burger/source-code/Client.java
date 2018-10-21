fa6cdfb15bfdce4107b602b5ef47dace5d2797ce
Saumya Goyal <saumya.iis@gmail.com>
2018-10-20 19:53:29 -0700

    Create Client.java

---
+++ lab7/counter-burger/source-code/Client.java
@@ -0,0 +1,13 @@
+  
+
+public class Client {
+
+    public static void runTest()
+    {
+        Component theOrder = BuildOrder.getOrder() ;
+        theOrder.printDescription();
+        
+        
+    }
+}
+ 
