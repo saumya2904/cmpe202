298d5791b33361834afec9566a83b46add040396
Saumya Goyal <saumya.iis@gmail.com>
2018-10-27 22:43:33 -0700

    Create App.java

---
+++ lab8/App.java
@@ -0,0 +1,63 @@
+/* (c) Copyright 2018 Paul Nguyen. All Rights Reserved */
+
+/**
+ *  Main App Class for Displaying Screen.
+ */
+public class App {
+
+    private Screen screen;
+    private CreditCardNum num;
+    private CreditCardExp exp;
+    private CreditCardCVC cvc;
+    private int count;
+
+    public App() {
+
+        screen = new Screen();
+        num = new CreditCardNum();
+        exp = new CreditCardExp();
+        cvc = new CreditCardCVC();
+        screen.addSubComponent(num);
+        screen.addSubComponent(exp);
+        screen.addSubComponent(cvc);
+
+        count = 0;
+
+    }
+
+    public String display() {
+        String output = "";
+        output += "========================================\n";
+        output += screen.name() + "\n";
+        output += "========================================\n";
+        output += "\n\n\n\n";
+        output += screen.display() + "\n";
+        output += "\n\n\n\n";
+        output += "========================================\n";
+        return output;
+    }
+
+    public void key(String ch) {
+    	if (ch.length()==1 && count<=23 && count>=0 ) {
+    	if ("x".equals(ch))
+    	{   
+    	
+    		System.out.println("count:" + count);
+    		screen.key(ch, count);
+    		count--;
+    		if (count<0) {
+    			count=0;
+    		}
+    	}
+    	else {
+        count++;
+        screen.key(ch, count);
+        if (count ==24)
+        	count =23;
+        }
+    	
+    }
+
+}
+}
+
